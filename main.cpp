4c789c1642162ac2b0c58319352dd4151cc0be2d
Rain Chen <87171700+CongJyu@users.noreply.github.com>
2023-02-04 22:31:16 +0800

    update test program

--- main.cpp
+++ main.cpp
@@ -6,7 +6,23 @@
 #include "myconio_mac.cpp"
 
 int main() {
-    goto_xy(5, 6);
-    std::cout << "Here." << std::endl;
+    clear_screen();
+    line(10, '|', '-');
+    text_color(1);
+    printf("The function CLEAR_SCREEN clears all characters from the\r\n");
+    text_color(2);
+    bold(1);
+    printf("cursor position to the end of the line within the\r\n");
+    text_color(3);
+    underline(1);
+    printf("current text window, without moving the cursor.\r\n");
+    text_color(4);
+    bold(0);
+    underline(0);
+    printf("Press any key to continue . . .");
+    goto_xy(14, 4);
+    getch();
+    clear_eol();
+    getch();
     return 0;
 }
